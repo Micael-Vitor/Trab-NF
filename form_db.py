21b884667c6b24b3e7cf73408f47a6a65029dce5
Micael-Vitor <162899165+Micael-Vitor@users.noreply.github.com>
2024-06-12 22:24:28 -0300

    form_db.py

--- form_db.py
+++ form_db.py
@@ -20,26 +20,3 @@ con.commit()
 con.close()
 
 
-
-def opcoes():
-    print('Criar NF: 1 \nEditar: 2 \nExcluir: 3 \nListar: 4')
-
-opcoes()
-
-OpcaoEscolhida = int(input('Digite a Opção Que gostaria: '))
-
-match OpcaoEscolhida:
-    #Criar NF
-    case 1:
-        print(f'Escolheu criar NF')
-    #Editar NF
-    case 2:
-        print('Escolheu a opção editar')
-    #Excluir NF
-    case 3:
-        print('Escolheu a opção excluir')
-    #Listar todas NF
-    case 4:
-        print('Escolheu a opção listar')
-    case _:
-        print('Opção invalida')
